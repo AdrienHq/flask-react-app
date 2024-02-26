36fff1bb4b9ca30f37fdfde1728cb65a89c8fbbc
AdrienHq <adrienhecq@gmail.com>
2024-02-26 15:50:31 +0100

    feat: docker image config for react front

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return ("Hello AHQ")
+    return ("Hello AHQ END")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
